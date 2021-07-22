036628dfd2dcfbff5a70fa6672e6dcd7220c7644
Steve Occhipinti <dev@stevenocchipinti.com>
2021-07-22 13:27:07 +1000

    Make the COOP and COEP headers work in dev too
    Rather than just configuring the vercel settings to use these custom headers,
    Next can use these settings for dev too.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ]
+  },
 }
