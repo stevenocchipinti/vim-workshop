526d6b6c437d97150252e964e2e115d810ead8fc
Steve Occhipinti <dev@stevenocchipinti.com>
2022-05-18 09:53:39 +1000

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,56 @@
+import { useEffect } from "react"
 import Head from "next/head"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import * as gtag from "../lib/gtag"
+
 import type { AppProps } from "next/app"
+
 import "../styles/globals.css"
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>Vim workshop</title>
-      <meta name="description" content="A place to work on your vim skills" />
-      <link rel="icon" type="image/svg+xml" href="/vim-workshop.svg" />
-    </Head>
-    <Component {...pageProps} />
-  </>
-)
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    router.events.on("hashChangeComplete", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("hashChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Head>
+        <title>Vim workshop</title>
+        <meta name="description" content="A place to work on your vim skills" />
+        <link rel="icon" type="image/svg+xml" href="/vim-workshop.svg" />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 export default App
